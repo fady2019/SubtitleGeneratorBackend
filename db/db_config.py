02701dc2949fd5d2bc3bf356d143c0fcee597423
Fady Emad <60940882+fady2019@users.noreply.github.com>
2024-08-05 12:13:14 +0300

    chore: configure the database

---
+++ db/db_config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, DeclarativeMeta
+
+load_dotenv()
+
+DB_LOGIN = os.getenv("DATABASE_LOGIN")
+DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DB_HOST = os.getenv("DATABASE_HOST")
+DB_PORT = os.getenv("DATABASE_PORT")
+DB_NAME = os.getenv("DATABASE_NAME")
+DB_URL = f"postgresql+psycopg2://{DB_LOGIN}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DB_ENGINE = create_engine(DB_URL)
+
+Base: DeclarativeMeta = declarative_base()
+
+
+def create_session():
+    return sessionmaker(bind=DB_ENGINE)
